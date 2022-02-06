8da246032a8afabb87c15a2d9f908f7664acfa61
EC2 Default User <ec2-user@ip-172-31-77-252.ec2.internal>
2022-02-06 02:47:04 +0000

    javascript

---
+++ main.js
@@ -0,0 +1,25 @@
+function clickbutton(target) {
+  let result = document.getElementById("result");
+  let target_value = target.innerHTML;
+  
+
+if (target_value == "AC") {
+    result.innerHTML = "0";
+ } else if (target_value == "=") {
+       result.innerHTML = eval(result.innerHTML);
+ } else {
+       if (result.innerHTML == "0") {
+         result.innerHTML = target_value;
+     } else if (result.innerHTML == "00") {
+         result.innerHTML = target_value;
+     } else if (result.innerHTML == "+") {
+         result.innerHTML = target_value;
+     } else if (result.innerHTML == "*") {
+         result.innerHTML = target_value;     
+     } else if (result.innerHTML == ".") {
+         result.innerHTML = eval("0." + target_value);
+     } else {
+         result.innerHTML += target_value; 
+     }
+   }
+}
